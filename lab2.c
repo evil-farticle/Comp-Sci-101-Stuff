47c5b1b5c2f510ef442be6b3145bb3e3214942ec
evil-farticle <surenj@clemson.edu>
2025-02-24 09:03:56 -0500

    Lab 2 for CPSC 1010
    Basically just print I LOVE PROGRAMMING! 10 times

---
+++ lab2.c
@@ -0,0 +1,33 @@
+/*<Suren Juliano>
+CPSC 1011, Lab Section
+Due 1/24/25
+Part 1 of Lab #2
+My first C program : Print "I love programming!" to the screen.
+*/
+
+#include <stdio.h>
+
+int main(void) {
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+	printf("I love programming!\n");
+     ;		
+	return 0;
+     }
